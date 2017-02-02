b67baad4d09365ee731c3915d5458535fa20a4dc
Ben Kuhl <ben.kuhl@realpage.com>
2017-02-02 14:39:45 -0500

    ci

--- tests/Feature/HealthcheckTest.php
+++ tests/Feature/HealthcheckTest.php
@@ -6,7 +6,7 @@ class HealthcheckTest extends TestCase
 {
     /** @test */
     public function canPingHealthcheck()
-    {//triggering ci
+    {
         $this->get('/healthcheck/'.env('HEALTHCHECK_TOKEN'))
             ->assertResponseOk();
     }
